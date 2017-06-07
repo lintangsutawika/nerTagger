d872bb813179d41e1b88aa3a1611adf16f8490c8
Lintang Sutawika <lintangsutawika@gmail.com>
2017-06-08 01:51:08 +0700

    able to change tags and save

--- content.js
+++ content.js
@@ -5,33 +5,44 @@ function emptyElement(element) {
     }
 }
 
-chrome.storage.local.get("text", function(string){
-    // alert("Got it");
-    var textString = string.text; 
-    console.log(textString);
+// chrome.storage.local.get("text", function(string){
+//     var tagName = string
+// });
 
-    emptyElement(document.body);
+chrome.storage.local.get(
+    ["text", 
+    "tagName1", "tagLabel1",
+    "tagName2", "tagLabel2",
+    "tagName3", "tagLabel3",], 
+    function(string){
+        // alert("Got it");
+        var textString = string.text; 
+        var tagName1 = string.tagName1; var tagLabel1 = string.tagLabel1;
+        var tagName2 = string.tagName2; var tagLabel2 = string.tagLabel2;
+        var tagName3 = string.tagName3; var tagLabel3 = string.tagLabel3;
+        
+        emptyElement(document.body);
 
-    var injectable = document.createElement('div');
-    injectable.innerHTML = textString;
-    injectable.classList.add("editable");
-    document.body.append(injectable);
+        var injectable = document.createElement('div');
+        injectable.innerHTML = textString;
+        injectable.classList.add("editable");
+        document.body.append(injectable);
 
-    var elements = document.querySelectorAll('.editable');
-    // editor = new MediumEditor(elements);
-    editor = new MediumEditor(elements, {
-        toolbar: {
-          buttons: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6']
-        },
-        extensions: {
-    //         // 'tag1':  new MediumButton({label:'PER', start:'&lt;', end:'&gt;'}),
-            'tag1':  new MediumButton({label:'PER', start:'&lt;ENAMEX TYPE=\"PERSON\"&gt;', end:'&lt;/ENAMEX&gt;'}),
-            'tag2':  new MediumButton({label:'LOC', start:'&lt;ENAMEX TYPE=\"LOCATION\"&gt;', end:'&lt;/ENAMEX&gt;'}),
-            'tag3':  new MediumButton({label:'ORG', start:'&lt;ENAMEX TYPE=\"ORGANIZATION\"&gt;', end:'&lt;/ENAMEX&gt;'}),
-            'tag4':  new MediumButton({label:'BRD', start:'&lt;ENAMEX TYPE=\"BRAND\"&gt;', end:'&lt;/ENAMEX&gt;'}),
-            'tag5':  new MediumButton({label:'TYP', start:'&lt;ENAMEX TYPE=\"TYPE\"&gt;', end:'&lt;/ENAMEX&gt;'}),
-            'tag6':  new MediumButton({label:'LVL', start:'&lt;ENAMEX TYPE=\"LEVEL\"&gt;', end:'&lt;/ENAMEX&gt;'}),
-        }
+        var elements = document.querySelectorAll('.editable');
+        // editor = new MediumEditor(elements);
+        editor = new MediumEditor(elements, {
+            toolbar: {
+              buttons: ['tag1', 'tag2', 'tag3', 'tag4', 'tag5', 'tag6']
+            },
+            extensions: {
+        //         // 'tag1':  new MediumButton({label:'PER', start:'&lt;', end:'&gt;'}),
+                'tag1':  new MediumButton({label:'PER', start:'&lt;ENAMEX TYPE=\"PERSON\"&gt;', end:'&lt;/ENAMEX&gt;'}),
+                'tag2':  new MediumButton({label:'LOC', start:'&lt;ENAMEX TYPE=\"LOCATION\"&gt;', end:'&lt;/ENAMEX&gt;'}),
+                'tag3':  new MediumButton({label:'ORG', start:'&lt;ENAMEX TYPE=\"ORGANIZATION\"&gt;', end:'&lt;/ENAMEX&gt;'}),
+                'tag4':  new MediumButton({label:tagLabel1, start:'&lt;ENAMEX TYPE=\"'+tagName1+'\"&gt;', end:'&lt;/ENAMEX&gt;'}),
+                'tag5':  new MediumButton({label:tagLabel2, start:'&lt;ENAMEX TYPE=\"'+tagName2+'\"&gt;', end:'&lt;/ENAMEX&gt;'}),
+                'tag6':  new MediumButton({label:tagLabel3, start:'&lt;ENAMEX TYPE=\"'+tagName3+'\"&gt;', end:'&lt;/ENAMEX&gt;'}),
+            }
     });
 });
 
--- popup.js
+++ popup.js
@@ -29,20 +29,46 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-document.addEventListener('DOMContentLoaded', function() {
-  var link = document.getElementById('urlbutton2');
+// document.addEventListener('DOMContentLoaded', function() {
+//   var link = document.getElementById('urlbutton2');
   
-  //Access data
-  chrome.storage.local.get("text", function(string){
-    // alert("Got it");
-    var textString = "<p>"+string.text+"</p>"; 
-    console.log(textString)
+//   //Access data
+//   chrome.storage.local.get("text", function(string){
+//     // alert("Got it");
+//     var textString = string.text; 
+//     // console.log(textString)
+//     //Event when tag button is clicked
+//       link.addEventListener('click', function() {
+//         exportToFile(textString);
+//      });           
+//   });
+// });
 
-    //Event when tag button is clicked
-      link.addEventListener('click', function() {
-        exportToFile(textString);
-     });           
-  });
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('urlbutton4');
+  chrome.storage.local.set(
+            {'tagName1': "NULL",'tagLabel1':"NULL",
+              'tagName2': "NULL",'tagLabel2':"NULL",
+              'tagName3': "NULL",'tagLabel3':"NULL"}, function(){
+            // alert("Success");
+        });
+  link.addEventListener('click', function() {
+    var tagName1_ = encodeURIComponent(document.getElementById('tagName1').value);
+    var tagLabel1_ = encodeURIComponent(document.getElementById('tagLabel1').value);
+    var tagName2_ = encodeURIComponent(document.getElementById('tagName2').value);
+    var tagLabel2_ = encodeURIComponent(document.getElementById('tagLabel2').value);
+    var tagName3_ = encodeURIComponent(document.getElementById('tagName3').value);
+    var tagLabel3_ = encodeURIComponent(document.getElementById('tagLabel3').value);
+    // console.log(tagName1_);
+    chrome.storage.local.set(
+        {'tagName1': tagName1_, 'tagLabel1':tagLabel1_,
+        'tagName2': tagName2_, 'tagLabel2':tagLabel2_,
+        'tagName3': tagName3_, 'tagLabel3':tagLabel3_}, function(){
+            // alert("Success");
+      });
+    });  
+  
+  
 });
 
 chrome.tabs.executeScript( {
@@ -53,5 +79,10 @@ chrome.tabs.executeScript( {
         chrome.storage.local.set({'text': selection[0]}, function(){
             // alert("Success");
         });
+
+          var link = document.getElementById('urlbutton2');
+          link.addEventListener('click', function() {
+          exportToFile(selection[0]);
+       });
      });
 
