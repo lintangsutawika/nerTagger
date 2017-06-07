58590af39bf9e1f43c7bd90a1dbb8724a6851251
Lintang Sutawika <lintangsutawika@gmail.com>
2017-06-07 18:54:41 +0700

    Can tag and save

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@ function sendMessage(url_link){
         chrome.extension.sendMessage({
         action: "openNewTab",
         url: url_link
-        // url: "test.html"
+        // url: "toTag.html"
     });
 }
 
@@ -13,35 +13,20 @@ function exportToFile(myString) {
 
 document.addEventListener('DOMContentLoaded', function() {
   var link = document.getElementById('urlbutton1');
-  
-  //Access data
-  chrome.storage.local.get("text", function(string){
-    // alert("Got it");
-    var textString = "<p>"+string.text+"</p>"; 
-    console.log(textString)
 
-    //Create editable class
-    var injectable = document.createElement('div');
-    injectable.innerHTML = textString;
-    injectable.classList.add("editable");
-    injectable.src = chrome.extension.getURL('toTag.html');
-    // injectable.src = "toTag.html";
-    document.getElementById('output').append(injectable);
-
-    //Event when tag button is clicked
-      link.addEventListener('click', function() {
-        var tab = ""
-        chrome.tabs.query({
-          active: true,
-          lastFocusedWindow: true
-        }, function(tabs) {      
-            // and use that tab to fill in out title and url
-            tab = tabs[0];
-            console.log(tab.url);
-            sendMessage(tab.url);
+  //Event when tag button is clicked
+  link.addEventListener('click', function() {
+    var tab = ""
+    chrome.tabs.query({
+      active: true,
+      lastFocusedWindow: true
+      }, function(tabs) {      
+          // and use that tab to fill in out title and url
+          tab = tabs[0];
+          console.log(tab.url);
+          sendMessage(tab.url);
         });
-     });           
-  });
+    });
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -66,7 +51,7 @@ chrome.tabs.executeScript( {
     function(selection) {
         document.getElementById("output").innerHTML = selection[0];
         chrome.storage.local.set({'text': selection[0]}, function(){
-            // alert("Success")
+            // alert("Success");
         });
      });
 
