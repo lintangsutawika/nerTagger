a3e315bc6ec7c0c13f1fc73b95654c13e26ef477
Lintang <lintangsutawika@gmail.com>
2017-12-22 02:01:59 +0700

    Development of new text selection from webpage method

--- event.js
+++ event.js
@@ -1,10 +1,65 @@
-// This function is called onload in the popup code
-function getPageDetails(callback) { 
-	// Inject the content script into the current page 
-	chrome.tabs.executeScript(null, { file: 'content.js' }); 
-	// Perform the callback when a message is received from the content script
-	chrome.runtime.onMessage.addListener(function(message)  { 
-		// Call the callback function
-		callback(message); 
-	}); 
-};
+/*
+*  Hover inspect extension for Chrome
+*  https://github.com/NV0/hover-inspect
+*/
+
+(function() {
+
+	var tabs = {};
+
+	var inspect = {
+		activate: function(id) {
+
+			this.id = id;
+			chrome.tabs.executeScript(this.id, {
+				file: 'prism.js'
+			});
+			chrome.tabs.executeScript(this.id, {
+				file: 'hoverinspect.js'
+			}, function() {
+				chrome.tabs.sendMessage(this.id, {
+					action: 'activate'
+				});
+			}.bind(this));
+
+			chrome.browserAction.setIcon({
+				tabId: this.id,
+				path: {
+					19: "icon_active.png"
+				}
+			});
+		},
+
+		deactivate: function() {
+
+			chrome.tabs.sendMessage(this.id, {
+				action: 'deactivate'
+			});
+
+			chrome.browserAction.setIcon({
+				tabId: this.id,
+				path: {
+					// 19: "icon.png"
+					19: "img/pencil.png"
+				}
+			});
+		}
+
+	};
+
+	function toggle(tab) {
+
+		if (!tabs[tab.id]) {
+			tabs[tab.id] = Object.create(inspect);
+			tabs[tab.id].activate(tab.id);
+		} else {
+			tabs[tab.id].deactivate();
+			for (var tabId in tabs) {
+				if (tabId == tab.id) delete tabs[tabId];
+			}
+		}
+	}
+
+	chrome.browserAction.onClicked.addListener(toggle);
+
+})();
